0e710bf2fbed4ef91b105ca614d45f4ac2337a21
Jakub Skrzypek <kuba81s@o2.pl>
2020-07-19 08:57:47 +0100

    CRud by Tania R start

---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DataBase App</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <h1>Simple Database App</h1>
+
+  <ul>
+    <li>
+      <a href="create.php"><strong>Create</strong></a> - add a user
+    </li>
+    <li>
+      <a href="read.php"><strong>Read</strong></a> - find a user
+    </li>
+  </ul>
+
+</body>
+
+</html>
