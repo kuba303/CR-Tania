f57f8cb436573c88da06bbed6c4685c4a5b6e043
Jakub Skrzypek <kuba81s@o2.pl>
2020-07-20 16:24:44 +0100

    add read.php html

---
+++ public/read.php
@@ -0,0 +1,14 @@
+<?php include "templates/header.php"; ?>
+
+<h2>Find user based on location</h2>
+
+<form action="" method="post">
+  <label for="location">Location</label>
+  <input type="text" name="locations" id="locations">
+  <input type="submit" value="View Results" name="submit">
+</form>
+
+<a href="index.php">Back to home</a>
+
+
+<?php include "templates/footer.php"; ?>
